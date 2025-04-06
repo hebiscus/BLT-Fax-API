e5b286139fd63046faed1353649f20f9753e30a5
hebiscus <akikopoland@gmail.com>
2025-04-06 21:09:55 +0200

    feat: add basic get faxes and psot fax endpoints

--- app.rb
+++ app.rb
@@ -1,1 +1,20 @@
 require 'sinatra'
+
+class FaxApp < Sinatra::Base
+  # @param [String] fax_number
+  # @param [File] file
+	post "/fax" do
+		p params
+		return "No file selected" unless params[:file] && (tempfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+
+  	target = "./faxes/fax-#{params[:fax_number]}-#{name}"
+		File.open(target, 'wb') {|f| f.write(tempfile.read)}
+    "File uploaded successfully!"
+	end
+
+	get "/faxes" do
+		erb :index, locals: {faxes: faxes}
+	end
+
+	run! if __FILE__ == $0
+end
