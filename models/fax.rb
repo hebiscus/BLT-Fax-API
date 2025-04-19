0e6904364d91eb4891ecda82bbfe3925e0f3a3e0
hebiscus <akikopoland@gmail.com>
2025-04-19 09:57:44 +0200

    feat: add user_token to fax model and complete the to_h

--- models/fax.rb
+++ models/fax.rb
@@ -1,21 +1,24 @@
 class Fax 
-  attr_reader :id, :file_path
+  attr_reader :id, :file_path, :user_token
   attr_accessor :receiver_number, :status
 
-  def initialize(id:, file_path:, receiver_number:, status:)
+  def initialize(id:, file_path:, receiver_number:, status:, user_token:)
     @id = id
     @file_path = file_path
     @receiver_number = receiver_number
     @status = status
+    @user_token = user_token
     @created_at = Time.now
   end
 
   def to_h
     {
       id: @id,
-      receiver_number: @receiver_number,
       file_path: @file_path,
-      status: @status
+      receiver_number: @receiver_number,
+      status: @status,
+      created_at: @created_at,
+      user_token: @user_token
     }
   end
 end
