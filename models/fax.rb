2e969452c11f137c857a6b3b64c018876aa729ae
hebiscus <akikopoland@gmail.com>
2025-04-19 20:41:15 +0200

    fix: correct fax to have default created_at

--- models/fax.rb
+++ models/fax.rb
@@ -2,13 +2,13 @@ class Fax
   attr_reader :id, :file_path, :user_token
   attr_accessor :receiver_number, :status
 
-  def initialize(id:, file_path:, receiver_number:, status:, user_token:)
+  def initialize(id:, file_path:, receiver_number:, status:, created_at: Time.now, user_token:)
     @id = id
     @file_path = file_path
     @receiver_number = receiver_number
     @status = status
     @user_token = user_token
-    @created_at = Time.now
+    @created_at = created_at
   end
 
   def content
