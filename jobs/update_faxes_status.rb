895f2a2faf531767be9f142a71184e4772080f3e
hebiscus <akikopoland@gmail.com>
2025-05-07 17:29:26 +0200

    feat: add cron job debug logs

--- jobs/update_faxes_status.rb
+++ jobs/update_faxes_status.rb
@@ -1,3 +1,7 @@
 require_relative "../repositories/faxes"
 
+File.open("/home/hebi/personal-projects/BLT-Fax-API/logs/cron_debug.log", "a") do |f|
+  f.puts "[#{Time.now}] Script started"
+end
+
 Repositories::Faxes.new.update_pending
