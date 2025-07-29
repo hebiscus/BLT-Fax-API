8e6a9783334c4af2e6446efc50df24d1fcf43a08
hebiscus <akikopoland@gmail.com>
2025-07-29 21:32:49 +0200

    feat: add render url host

--- config/sinatra_patch.rb
+++ config/sinatra_patch.rb
@@ -4,7 +4,7 @@ module Sinatra
   class Base
     configure do
       set :bind, '0.0.0.0' 
-      set :host_authorization, { permitted_hosts: ["127.0.0.1", "localhost", "blt-shop.onrender.com"] }
+      set :host_authorization, { permitted_hosts: ["127.0.0.1", "localhost", "blt-shop.onrender.com", "blt-fax-api.onrender.com"] }
     end
   end
 end
